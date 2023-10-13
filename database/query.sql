b74f7873b06a9ec85d3cdf1108eef5e167f91643
Andrea Barchietto <andreabarchietto01@gmail.com>
2023-10-13 15:28:38 +0200

    init

--- database/query.sql
+++
@@ -1,7 +0,0 @@
-select partitaIvaAcquirente, sum(totale) - (select
-    sum(esigenzaAcqua.totale) from esigenzaAcqua
-    where esigenzaAcqua.partitaIva = acquisto.partitaIvaAcquirente
-    group by esigenzaAcqua.partitaIva
-) 
-from acquisto
-group by partitaIvaAcquirente

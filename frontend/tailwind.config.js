39a781d21ee5673653dbb4515e830b25e2b91c44
Andrea Barchietto <andreabarchietto01@gmail.com>
2023-10-13 15:14:37 +0200

    init frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "Pages/**/*.cshtml",
+    "**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

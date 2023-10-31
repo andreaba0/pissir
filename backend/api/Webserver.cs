4711b2043919302ab2757d847a8231f325f89942
Andrea Barchietto <andreabarchietto01@gmail.com>
2023-10-31 17:34:00 +0100

    add rest routes

--- backend/api/Webserver.cs
+++ backend/api/Webserver.cs
@@ -94,34 +94,50 @@ public class Webserver
                 return;
             }
             context.Response.StatusCode = 200;
-            await context.Response.WriteAsync("Hello World!");
+            await context.Response.WriteAsync("OK");
         });
 
         app.MapPost("/api/water/buy", async context =>
         {
             context.Response.StatusCode = 200;
-            await context.Response.WriteAsync("Hello World!");
+            await context.Response.WriteAsync("OK");
         });
         app.MapPost("/api/water/offer", async context =>
         {
             context.Response.StatusCode = 200;
-            await context.Response.WriteAsync("Hello World!");
+            await context.Response.WriteAsync("OK");
         });
         app.MapPost("/api/company/field", async context =>
         {
             context.Response.StatusCode = 200;
-            await context.Response.WriteAsync("Hello World!");
+            await context.Response.WriteAsync("OK");
         });
-        app.MapDelete("/api/company/field", async context =>
+        app.MapDelete("/api/company/{field}", async context =>
         {
             context.Response.StatusCode = 200;
-            await context.Response.WriteAsync("Hello World!");
+            await context.Response.WriteAsync("OK");
         });
-        app.MapGet("/api/analytics/field", async context =>
+        app.MapGet("/api/analytics/{field}/{type}", async context =>
         {
             context.Response.StatusCode = 200;
-            await context.Response.WriteAsync("Hello World!");
+            await context.Response.WriteAsync("OK");
         });
+        app.MapPost("/api/object/{type}", async context => {
+            context.Response.StatusCode=200;
+            await context.Response.WriteAsync("OK");
+        });
+        app.MapDelete("/api/object/{id}", async context => {
+            context.Response.StatusCode=200;
+            await context.Response.WriteAsync("OK");
+        });
+        app.MapPost("/api/company/field", async context => {
+            context.Response.StatusCode=200;
+            await context.Response.WriteAsync("OK");
+        });
+        app.MapPost("/api/water/limit/{company}", async context => {
+            context.Response.StatusCode=200;
+            await context.Response.WriteAsync("OK");
+        })
 
         app.Run();
     }

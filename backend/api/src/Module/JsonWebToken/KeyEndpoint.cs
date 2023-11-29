8c7e179c439354ecde401af6b0e0c47563ca6143
Andrea Barchietto <andreabarchietto01@gmail.com>
2023-11-30 00:17:08 +0100

    add HttpClient

--- backend/api/src/Module/JsonWebToken/KeyEndpoint.cs
+++ backend/api/src/Module/JsonWebToken/KeyEndpoint.cs
@@ -1,5 +1,13 @@
 namespace Module.JsonWebToken;
 
 public class KeyEndpoint {
-    
+    private readonly HttpClient _httpClient;
+
+    public KeyEndpoint(HttpClient httpClient) {
+        _httpClient = httpClient;
+    }
+
+    public async Task QueryKeys(CancellationToken ct) {
+        
+    }
 }

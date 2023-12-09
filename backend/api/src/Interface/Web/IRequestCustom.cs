ce46648071b1326d9c9ebdca3b6d977a8cf855ba
Andrea Barchietto <andreabarchietto01@gmail.com>
2023-12-09 23:20:46 +0100

    init webserver thread

---
+++ backend/api/src/Interface/Web/IRequestCustom.cs
@@ -0,0 +1,5 @@
+namespace Web;
+public interface IRequestCustom
+{
+    public NameValueCollection Headers { get; set; }
+}
---
+++ backend/api/src/Web/RequestCustom.cs
@@ -0,0 +1,5 @@
+namespace Web;
+
+public class RequestCustom: IRequestCustom {
+
+}

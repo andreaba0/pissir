3599930d7a5d38a6abd29d2c37b63ed30ae646a1
andreaba0 <andreabarchietto01@gmail.com>
2024-07-07 03:02:43 +0200

    fix database schema, add Dockerfile

--- database/api/alter.sql
+++ database/api/alter.sql
@@ -1,27 +1,3 @@
-alter table allowed_audience
-add foreign key (registered_provider) references registered_provider(provider_name);
-
-alter table user_account
-add foreign key (registered_provider) references registered_provider(provider_name);
-
-alter table person
-add foreign key (account_id) references user_account(id);
-
-alter table person
-add foreign key (person_role) references user_role(role_name);
-
-alter table person_fa
-add foreign key (account_id, role_name) references person(account_id, person_role);
-
-alter table person_fa
-add foreign key (company_vat_number) references company_far(vat_number);
-
-alter table person_wa
-add foreign key (account_id, role_name) references person(account_id, person_role);
-
-alter table person_wa
-add foreign key (company_vat_number) references company_wsp(vat_number);
-
 alter table company
 add foreign key (industry_sector) references industry_sector(sector_name);
 
@@ -31,21 +7,24 @@ add foreign key (vat_number, industry_sector) references company(vat_number, ind
 alter table company_wsp
 add foreign key (vat_number, industry_sector) references company(vat_number, industry_sector);
 
-alter table offer add foreign key (vat_number) references company(vat_number);
-
+alter table offer add foreign key (company_industry_sector) references industry_sector(sector_name);
+alter table offer add foreign key (vat_number) references company_wsp(vat_number);
+ 
 alter table buy_order add foreign key (offer_id) references offer(id);
 alter table buy_order add foreign key (farm_field_id) references farm_field(id);
 
-alter table farm_field add foreign key (vat_number) references company(vat_number);
+alter table daily_water_limit add foreign key (vat_number) references company_far(vat_number);
+
+alter table farm_field add foreign key (vat_number) references company_far(vat_number);
 alter table farm_field add foreign key (irrigation_type) references irrigation_type(irrigation_name);
 
-alter table object_logger add foreign key (sensor_type) references sensor_type(id);
 alter table object_logger add foreign key (farm_field_id) references farm_field(id);
 
 alter table umdty_sensor_log add foreign key (object_id) references object_logger(id);
-alter table umdty_sensor_log add foreign key (sensor_type) references sensor_type(id);
+alter table umdty_sensor_log add foreign key (object_id, object_type) references object_logger(id, object_type);
 
 alter table tmp_sensor_log add foreign key (object_id) references object_logger(id);
-alter table tmp_sensor_log add foreign key (sensor_type) references sensor_type(id);
+alter table tmp_sensor_log add foreign key (object_id, object_type) references object_logger(id, object_type);
 
 alter table actuator_log add foreign key (object_id) references object_logger(id);
+alter table actuator_log add foreign key (object_id, object_type) references object_logger(id, object_type);
--- database/api/schema.sql
+++ database/api/schema.sql
@@ -1,61 +1,11 @@
-create table allowed_audience (
-    registered_provider text not null,
-    audience text not null,
-    primary key (registered_provider, audience)
-);
-
-create table registered_provider (
-    provider_name text primary key,
-    configuration_uri text not null unique,
-    is_active boolean not null default true
-);
-
-create table user_account (
-    id bigserial primary key,
-    registered_provider text not null,
-    sub text not null,
-    unique (registered_provider, sub)
-);
-
 create table industry_sector (
     sector_name varchar(3) primary key check(sector_name in ('WSP', 'FAR'))
-);
-
-create table user_role (
-    role_name varchar(2) primary key check(role_name in ('WA', 'FA'))
-);
-
-create table person(
-    global_id uuid unique not null default gen_random_uuid(),
-    tax_code varchar(16) not null unique,
-    account_id bigint primary key,
-    given_name text not null,
-    family_name text not null,
-    email text not null,
-    person_role varchar(2) not null,
-    unique (account_id, person_role)
-);
-
-create table person_fa (
-    account_id bigint primary key,
-    role_name varchar(2) not null check(role_name = 'FA'),
-    company_vat_number varchar(11) not null
-);
-
-create table person_wa (
-    account_id bigint primary key,
-    role_name varchar(2) not null check(role_name = 'WA'),
-    company_vat_number varchar(11) not null
 );
 
 create table company(
     vat_number varchar(11) primary key,
     industry_sector varchar(3) not null,
-    company_name text,
-    working_email_address text,
-    working_phone_number varchar(10),
-    working_address text,
-    unique (vat_number, industry_sector)
+    unique(vat_number, industry_sector)
 );
 
 create table company_far (
@@ -71,18 +21,38 @@ create table company_wsp (
 create table offer(
     id varchar(26) primary key,
     vat_number varchar(11) not null,
-    company_industry_sector varchar(2) not null check(company_industry_sector = 'WA'),
+    company_industry_sector varchar(3) not null check(company_industry_sector = 'WSP'),
     publish_date date not null,
-    price_liter float not null,
-    qty float not null,
+    price_liter float not null check(price_liter >= 0),
+    available_liters float not null check(available_liters >= 0),
+    purchased_liters float not null check(purchased_liters >= 0) default 0,
     unique(vat_number, publish_date, price_liter)
 );
+
+alter table offer
+add constraint purchase_coherence check (available_liters >= purchased_liters and available_liters - purchased_liters >= 0);
+
 create table buy_order(
     offer_id varchar(26),
     farm_field_id varchar(26),
-    qty float not null,
+    qty float not null check(qty >= 0),
     primary key (offer_id, farm_field_id)
 );
+
+create table daily_water_limit(
+    vat_number varchar(11) not null,
+    company_industry_sector varchar(3) not null check(company_industry_sector = 'FAR'),
+    consumption_sign smallint not null check(consumption_sign in (-1, 1)), --: -1 for unlimited [-inf; 0], 1 for limited [0; +limit]
+    available float not null check (available >= 0),
+    consumed float not null check (consumed >= 0),
+    on_date date not null,
+    primary key (vat_number, on_date)
+);
+
+alter table daily_water_limit 
+add constraint consumption_coherence check (available >= (consumed * consumption_sign) and available - (consumed * consumption_sign) >= 0);
+
+
 create table farm_field(
     id varchar(26) primary key,
     vat_number varchar(11),
@@ -91,32 +61,35 @@ create table farm_field(
     crop_type text not null,
     irrigation_type text not null
 );
+
 create table irrigation_type(
     irrigation_name text primary key
 );
-create table sensor_type(
-    id text primary key
-);
+
 create table object_logger(
     id varchar(26) primary key,
-    sensor_type text not null,
+    object_type text not null check(object_type in ('UMDTY', 'TMP', 'ACTUATOR')),
     farm_field_id varchar(26) not null,
-    unique (id, sensor_type)
+    unique (id, object_type)
 );
 create table umdty_sensor_log(
     object_id varchar(26),
-    sensor_type text,
+    object_type text not null check(object_type = 'UMDTY'),
     log_time timestamptz not null,
-    umdty float not null check (umdty >= 0 and umdty <= 100)
+    umdty float not null check (umdty >= 0 and umdty <= 100),
+    primary key (object_id, log_time)
 );
 create table tmp_sensor_log(
     object_id varchar(26),
-    sensor_type text,
+    object_type text not null check(object_type = 'TMP'),
     log_time timestamptz not null,
-    tmp float not null
+    tmp float not null,
+    primary key (object_id, log_time)
 );
 create table actuator_log(
     object_id varchar(26),
     log_time timestamptz not null,
-    is_active boolean not null
+    is_active boolean not null,
+    object_type text not null check(object_type = 'ACTUATOR'),
+    primary key (object_id, log_time)
 );

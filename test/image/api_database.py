a015a981f48c8ba71d225ab582c7b1172679fdb1
andreaba0 <andreabarchietto01@gmail.com>
2024-07-07 02:58:47 +0200

    add run method

--- test/image/api_database.py
+++ test/image/api_database.py
@@ -9,4 +9,19 @@ client = client.get_client()
 class api_database:
     name = api_database_config["image_name"]
 
-    
+    def run(name_with_tag, env_list):
+        print("Creating container")
+        container_name = f"{api_database_config['image_name']}_{int(time.time())}"
+        container = client.containers.run(
+            name_with_tag,
+            #name=container_name,
+            environment=env_list,
+            ports={f"{api_database_config['internal_port']}/tcp": api_database_config['exposed_port']},
+            detach=True,
+            labels={
+                "com.pissir.env": "testing",
+                "com.pissir.role": name_with_tag.split(":")[0],
+            },
+            remove=False
+        )
+        return container
